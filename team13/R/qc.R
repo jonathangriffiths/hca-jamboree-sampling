4e1702782e3faaf6d61139f37b68600fddbab799
Elad Chomsky <eladch@Elads-MacBook-Pro.local>
2018-04-10 21:32:31 -0400

    Added option to QC based on max umis

--- team13/R/qc.R
+++ team13/R/qc.R
@@ -1,9 +1,12 @@
 #Functions should take a count matrix and return a column-subsetted counts matrix
 #after QC purposes.
 
-qc_simple = function(count_matrix, mito.frac = 0.03, min.umis = 1000, gene_df = genes){
+qc_simple = function(count_matrix, mito.frac = 0.03, min.umis = 1000, max.umis=NULL) {
   count_matrix = count_matrix[, Matrix::colSums(count_matrix) >= min.umis]
-  mt.genes = grepl("^MT-", gene_df[,1])
+  if (!is.null(max.umis)) {
+    count_matrix = count_matrix[, Matrix::colSums(count_matrix) <= max.umis]
+  }
+  mt.genes = grepl("^MT-", rownames(count_matrix))
   mito_frac = Matrix::colSums(count_matrix[mt.genes,])/Matrix::colSums(count_matrix)
   count_matrix = count_matrix[,mito_frac < mito.frac]
   return(count_matrix)
