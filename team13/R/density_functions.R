b72880420341b2f54828c714f4a1ce18dd5be285
Jonathan Griffiths <jonathan.griffiths.94@gmail.com>
2018-06-07 10:43:04 +0100

    Added simple kernel density

--- team13/R/density_functions.R
+++ team13/R/density_functions.R
@@ -1,11 +1,29 @@
-get_kth_distances = function(dim_red, k = 5){
+#TODO: local dimension estimation
+
+get_k_distances = function(dim_red, k = 5){
   require(FNN)
   knns = get.knn(data = dim_red, k = k+1)
-  return(knns$nn.dist[,ncol(knns$nn.dist)])
+  return(knns$nn.dist[,-1])
 }
 
-get_cell_densities = function(dim_red, k = 5){
+get_kth_distances = function(dim_red, k = 5){
+  dist = get_k_distances(dim_red)
+  return(dist[,ncol(knns$dist)])
+}
+
+get_knn_densities = function(dim_red, k = 5){
   distances = get_kth_distances(dim_red, k=k)
   logvolumes = log(distances) * ncol(dim_red)
   return(-logvolumes)
+}
+
+normal_kernel_density = function(vec){
+  #constants excluded, as these are always relative
+  return(sum(exp(-1/2 * vec ^ 2)))
+}
+
+get_kernel_densities = function(dim_red, k = 50, kernel_function = normal_kernel_density){
+  distances = get_k_distances(dim_red, k=k)
+  densities = apply(distances, 1, kernel_function)
+  return(densities)
 }
